1e210538a33de1ffffbe94932d97cd1cbfc32c45
Davenchy <firon1222@gmail.com>
2017-05-27 21:39:30 +0200

    New Methods and HowToUse
    Add new methods
    Simple Tutorial was added

--- README.md
+++
@@ -1,1 +0,0 @@
-# JST Javascript Terminal
--- Terminal.js
+++ Terminal.js
@@ -1,4 +1,4 @@
-    /**
+/**
  * JST (Javascript Terminal)
  * Version 1.0.0.0
  * Created By Davenchy
@@ -6,6 +6,9 @@
  */
 
 function Terminal() {
+    try {
+        $("<div>Check Jquery");
+    } catch (e) { alert("jquery.js is missing"); }
     this.view;
     this.inputBox;
     this.keys = { 13:"enter", 116:"f5", 123:"f12", 38:"up", 40:"down" };
@@ -14,7 +17,10 @@ function Terminal() {
     this.historyIndex = 0;
     this.outputType = {info:"#2196f3", warn:"#ff9800", message:"#4caf50", error:"#f44336"};
     this.framework = {
+        //  Terminal Object
         parent:this,
+
+        //  Clear All Terminal Elements
         clear:function(input) {
             while(this.parent.view.children().length != 0) {
                 for(var i = 0; i < this.parent.view.children().length; i++) {
@@ -23,15 +29,26 @@ function Terminal() {
             }
             this.parent.drawInputBox();
         },
+
+
+        //  Show About Message
         about:function(input) {
+            this.parent.eline();
+            this.parent.hline();
             this.parent.output("Javascript Terminal", this.parent.outputType.info);
             this.parent.output("Version 1.0.0.0", this.parent.outputType.info);
             this.parent.output("Created By Davenchy", this.parent.outputType.info);
+            this.parent.hline();
+            this.parent.eline();
         },
+
+        //  Print string [data]
         echo:function(data) {
             if (data.length > 0) { this.parent.output(data, this.parent.outputType.info); }
             else { throw "Tip : echo Message To Print"; }
         },
+
+        //  Split Args by spaces and return array (Under Develop)
         splitArgs:function(argsStr) {
             var data = argsStr.split(" ");
             var args = [];
@@ -42,23 +59,39 @@ function Terminal() {
         }
     };
 }
+
+//  Select Terminal Element
 Terminal.prototype.setView = function(data) {
     this.view = $(data);
 }
+
+//  Append InputBox to the terminal
 Terminal.prototype.drawInputBox = function() {
     this.inputBox = $("<input type='text' class='InputBox'>")
     this.view.append(this.inputBox);
     this.inputBox.focus();
 }
-Terminal.prototype.draw = function(data) {
+
+//  Append html code to Terminal
+Terminal.prototype.create = function(data) {
     this.view.append(data);
     this.view.append(this.inputBox);
     this.inputBox.val("");
     this.inputBox.focus();
 }
+
+//  Print some string in Terminal
 Terminal.prototype.output = function(text, type) {
-    this.draw("<div class='Line' style='color: " + type + ";'>" + text + "</div>");
+    this.create("<div class='Line' style='color: " + type + ";'>" + text + "</div>");
 }
+
+//  the same as <br>    [Empity Line]
+Terminal.prototype.eline = function(text, type) { this.create("<br>"); }
+
+//  the same as <hr>    [Horizontal Line]
+Terminal.prototype.hline = function(text, type) { this.create("<hr>"); }
+
+//  On key down event method
 Terminal.prototype.onKeyDown = function(e) {
     //console.log(e.keyCode);
     //console.log(this.inputBox.val());
@@ -84,6 +117,8 @@ Terminal.prototype.onKeyDown = function(e) {
         }
     }
 }
+
+//  On InputBox change event method
 Terminal.prototype.input = function() {
     var string = this.inputBox.val();
     if (string != "") { 
@@ -98,9 +133,10 @@ Terminal.prototype.input = function() {
         this.output(string, this.outputType.message);
         this.compilor(string);
     }
-    //console.log(this.history);
     this.historyIndex = this.history.length;
 }
+
+//  Method to check called codes from the Terminal
 Terminal.prototype.compilor = function(code) {
     var cmd = code.split(" ")[0];
     var pars = "";
